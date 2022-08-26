943d1b306d077650d38c9fb5125e57883f91a216
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-08-26 14:58:35 +0200

    Header saker igen

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,15 +1,28 @@
 <template>
   <div
-    class="h-16 py-4 bg-gray-800 text-white flex justify-between items-center"
+    class="
+      h-16
+      py-4
+      px-2
+      bg-gray-800
+      text-white
+      flex
+      justify-between
+      items-center
+    "
   >
     <div class="flex flex-row mx-2 items-center">
       <p class="text-xl flex mr-4">Fire Experts</p>
       <img src="~static/flame icon.png" class="w-6" />
     </div>
-    <div>
+    <div class="flex flex-row mx-2 items-center">
+      <img
+        src="~static/shopping-cart-icon-png-transparent-28.jpg"
+        class="w-8 mx-3 filter invert"
+      />
       <img
         src="~static/profile-png-icon-1.jpg"
-        class="w-10 mx-3 filter invert"
+        class="w-8 mx-3 filter invert"
       />
     </div>
   </div>
