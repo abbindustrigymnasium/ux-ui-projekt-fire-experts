accebaeab36c011928a9cfdc3467f388ff276a1c
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-08-26 14:51:37 +0200

    Jobbade på headern

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="h-14 bg-gray-800 text-white flex-no-wrap">
+  <div
+    class="h-16 py-4 bg-gray-800 text-white flex justify-between items-center"
+  >
+    <div class="flex flex-row mx-2 items-center">
+      <p class="text-xl flex mr-2">Fire Experts</p>
+      <img src="~static/flame icon.png" class="w-8" />
+    </div>
     <div>
-      <p class="text-lg">Fire Experts</p>
-      <img src="https://www.iconsdb.com/icons/preview/white/fire-2-xxl.png" />
+      <img
+        src="~static/profile-png-icon-1.jpg"
+        class="w-10 mx-3 filter invert"
+      />
     </div>
   </div>
 </template>
