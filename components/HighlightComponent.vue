db7b85b11a6aeabb4aa467ad8e46d52d167af78e
abbangtor <angla.tornros@abbgymnasiet.se>
2022-09-02 14:01:31 +0200

    ?

---
+++ components/HighlightComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex bg-gray-300 px-10" >
+    HEJ
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,14 @@
   <div>
     <div>hej</div>
     <Tutorial></Tutorial>
+    <HighlightComponent/>
   </div>
 </template>
 
 <script>
+import HighlightComponent from '../components/HighlightComponent.vue'
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { HighlightComponent }
 }
 </script>
