bba9a1d93c3e43bdc7aff95ed9b4210e8fcfc808
abbangtor <angla.tornros@abbgymnasiet.se>
2022-09-02 14:45:06 +0200

    Fixar highlights layout

--- components/HighlightComponent.vue
+++ components/HighlightComponent.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="flex bg-gray-300 px-10" >
-    HEJ
+<div>
+
+<div  class="flex justify-between">
+
+  <div class="bg-gray-300 w-3/6">
+   <div>HEJ</div> 
   </div>
+
+<div class="bg-gray-300 w-2/6">
+<div class="bg-gray-300">1</div>
+<div class="bg-gray-300">2</div>
+<div class="bg-gray-300">3</div>
+</div>
+ 
+</div>
+
+</div>
+
 </template>
 
 <script>
