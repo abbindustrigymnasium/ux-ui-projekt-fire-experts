e5479db16f987b5c0e65667226d04a037127d35a
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-09-02 14:43:15 +0200

    Footer och padding i layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-700 min-h-screen text-white flex flex-col">
     <HeaderComponent></HeaderComponent>
-    <div class="min-h-screen px-10">
+    <div class="min-h-screen px-10 md:px-20">
       <nuxt></nuxt>
     </div>
     <FooterComponent></FooterComponent>
