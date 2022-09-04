3e24d14cfc4bf3187bb710157c4e0b70e7cefb03
abbangtor <angla.tornros@abbgymnasiet.se>
2022-09-04 19:10:29 +0200

    .

--- components/HighlightComponent.vue
+++ components/HighlightComponent.vue
@@ -3,14 +3,14 @@
 
 <div  class="flex justify-between">
 
-  <div class="bg-gray-300 w-3/6">
+  <div class="bg-gray-800 w-7/12 rounded-2xl flex justify-center">
    <div>HEJ</div> 
   </div>
 
-<div class="bg-gray-300 w-2/6">
-<div class="bg-gray-300">1</div>
-<div class="bg-gray-300">2</div>
-<div class="bg-gray-300">3</div>
+<div class="bg-gray-200 w-4/12 h-60 flex flex-col justify-between">
+    <div class="bg-gray-800 h-1/4 rounded-2xl">1</div>
+    <div class="bg-gray-800 h-1/4 rounded-2xl">2</div>
+    <div class="bg-gray-800 h-1/4 rounded-2xl">3</div>
 </div>
  
 </div>
