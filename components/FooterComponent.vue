e4fc4cea573db815f4940aa6092f6519524d260d
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-09-07 10:02:08 +0200

    saknade en sluttag i en div

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="
+  <div
+    class="
       h-10
       md:h-44
       py-4
@@ -9,7 +10,8 @@
       flex
       justify-between
       mt-5
-    ">
+    "
+  >
     <div class="ml-4 px-2 flex flex-col justify-evenly text-md md:text-lg">
       <p class="hover:underline cursor-pointer">Om oss</p>
       <p class="hover:underline cursor-pointer">Kontakta oss</p>
@@ -17,41 +19,45 @@
       <p class="hover:underline cursor-pointer">Ännu en länk</p>
     </div>
     <div class="flex flex-col items-center">
-
       <div>
         <img src="~static/flame icon.png" class="w-20 items-center" />
       </div>
-      <div>
-        back to top
-      </div>
+      <div>back to top</div>
       <div class="h-max flex align-center">
         <img src="~static/flame icon.png" class="w-20 self-center" />
-
       </div>
       <div class="flex flex-row mx-2 items-center">
         <a href="https://mobile.twitter.com/abbgymnasiet">
-          <img src="~static/twitter logo.png" class="
-            w-10
-            mx-4
-            transform
-            transition
-            duration-200
-            ease-in-out
-            hover:scale-110
-          " />
+          <img
+            src="~static/twitter logo.png"
+            class="
+              w-10
+              mx-4
+              transform
+              transition
+              duration-200
+              ease-in-out
+              hover:scale-110
+            "
+          />
         </a>
         <a href="https://sv-se.facebook.com/abbgymnasiet">
-          <img src="~static/facebook logo.png" alt="" class="
-            w-12
-            mx-4
-            filter
-            invert
-            transform
-            transition
-            duration-200
-            ease-in-out
-            hover:scale-110
-          " /></a>
+          <img
+            src="~static/facebook logo.png"
+            alt=""
+            class="
+              w-12
+              mx-4
+              filter
+              invert
+              transform
+              transition
+              duration-200
+              ease-in-out
+              hover:scale-110
+            "
+        /></a>
       </div>
     </div>
+  </div>
 </template>
