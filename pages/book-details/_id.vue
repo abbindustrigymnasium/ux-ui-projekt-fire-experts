b7587454be94446d2d6c9fd019e11eb70e60e0c0
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-09-07 14:25:27 +0200

    id page saker

--- pages/book-details/_id.vue
+++ pages/book-details/_id.vue
@@ -1,28 +1,42 @@
 <template>
-  <div>
-    <!-- <div>
-      <p>{{ book.title }} av {{ book.author }}</p>
-    </div> -->
-    <p v-if="$fetchState.pending">Loading....</p>
+  <div class="bg-gray-600 rounded-3xl p-10">
+    <div>
+      <h1>{{ currentBook.title }}</h1>
+    </div>
+    <div class="mb-10">
+      <p>{{ bookId }}</p>
+      <p>{{ currentBook.title }}</p>
+      <img :src="currentBook.img" />
+    </div>
+    <!-- <p v-if="$fetchState.pending">Loading....</p>
     <p v-else-if="$fetchState.error">Error while fetching books</p>
     <ul v-else>
       <li></li>
       <li v-for="(book, index) in books" :key="index">
         {{ book.title }} || {{ book.id }}
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
+
 <script>
 export default {
+  mounted () {
+    console.log(this.$route.params.id)
+  },
   data () {
     return {
-      books: []
+      // books: [],
+      bookId: this.$route.params.id,
+      currentBook: [],
     }
   },
   async fetch () {
-    this.books = await fetch(
-      'http://localhost:5000/books'
+    // this.books = await fetch(
+    //   'http://localhost:5000/books'
+    // ).then(res => res.json()),
+    this.currentBook = await fetch(
+      'http://localhost:5000/books/' + this.$route.params.id
     ).then(res => res.json())
   }
 }
