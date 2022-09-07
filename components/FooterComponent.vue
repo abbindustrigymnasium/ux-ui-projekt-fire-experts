6fd045dd700eda51b0145707e60f57d12f4aa5ac
abbcecek <cecilia.ek@abbgymnasiet.se>
2022-09-07 12:47:20 +0200

    la till id page och databas

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -18,46 +18,40 @@
       <p class="hover:underline cursor-pointer">Våran databas</p>
       <p class="hover:underline cursor-pointer">Ännu en länk</p>
     </div>
-    <div class="flex flex-col items-center">
-      <div>
-        <img src="~static/flame icon.png" class="w-20 items-center" />
-      </div>
-      <div>back to top</div>
-      <div class="h-max flex align-center">
-        <img src="~static/flame icon.png" class="w-20 self-center" />
-      </div>
-      <div class="flex flex-row mx-2 items-center">
-        <a href="https://mobile.twitter.com/abbgymnasiet">
-          <img
-            src="~static/twitter logo.png"
-            class="
-              w-10
-              mx-4
-              transform
-              transition
-              duration-200
-              ease-in-out
-              hover:scale-110
-            "
-          />
-        </a>
-        <a href="https://sv-se.facebook.com/abbgymnasiet">
-          <img
-            src="~static/facebook logo.png"
-            alt=""
-            class="
-              w-12
-              mx-4
-              filter
-              invert
-              transform
-              transition
-              duration-200
-              ease-in-out
-              hover:scale-110
-            "
-        /></a>
-      </div>
+    <div class="h-max flex items-center">
+      <img src="~static/flame icon.png" class="w-20 items-center" />
+    </div>
+    <div class="flex flex-row mx-2 items-center">
+      <a href="https://mobile.twitter.com/abbgymnasiet">
+        <img
+          src="~static/twitter logo.png"
+          class="
+            w-10
+            mx-4
+            transform
+            transition
+            duration-200
+            ease-in-out
+            hover:scale-110
+          "
+        />
+      </a>
+      <a href="https://sv-se.facebook.com/abbgymnasiet">
+        <img
+          src="~static/facebook logo.png"
+          alt=""
+          class="
+            w-12
+            mx-4
+            filter
+            invert
+            transform
+            transition
+            duration-200
+            ease-in-out
+            hover:scale-110
+          "
+      /></a>
     </div>
   </div>
 </template>
