1e996bae315ff7146145c9de99c0f780dd01d93f
abbangtor <angla.tornros@abbgymnasiet.se>
2022-09-07 13:28:34 +0200

    Data bas

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    <HighlightComponent/>
-  </div>
+        <ul> 
+          <li v-for="user in users" :key="user.id">{{user.name}} </li>
+        </ul> 
+        
+    <!-- <HighlightComponent/> -->
+       
+        
+  
+  
 </template>
 
 <script>
-import HighlightComponent from '../components/HighlightComponent.vue'
+  
+import { async } from 'q'
+// import HighlightComponent from '../components/HighlightComponent.vue'
 export default {
     name: "IndexPage",
-    components: { HighlightComponent }
-}
+    //  components: { HighlightComponent },
+    async asyncData({$axios}){
+      try {
+        let users = await $axios.$get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        return {users};
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    data(){
+      return {
+        users: [],
+      };
+
+      },
+    };
+
+
+
+
 </script>
